6f312dc2acdae635b3700009a27a25b405e51a08
The Eye Guy <thenceroth@liberty.edu>
2026-07-21 13:11:45 -0400

    Update scene-command.js

--- scene-command.js
+++ scene-command.js
@@ -122,6 +122,127 @@ const sceneCommand = new SlashCommandBuilder()
 
   .addSubcommand((subcommand) =>
     subcommand
+      .setName('register-additional')
+      .setDescription('Register another scene inside a reused RP thread.')
+
+      .addStringOption((option) =>
+        option
+          .setName('title')
+          .setDescription('The title of the additional scene.')
+          .setMaxLength(100)
+          .setRequired(true)
+      )
+
+      .addStringOption((option) =>
+        option
+          .setName('location')
+          .setDescription('Where the additional scene takes place.')
+          .setMaxLength(100)
+          .setRequired(true)
+      )
+
+      .addStringOption((option) =>
+        option
+          .setName('characters')
+          .setDescription('Characters involved, separated by commas.')
+          .setMaxLength(1000)
+          .setRequired(true)
+      )
+
+      .addStringOption((option) =>
+        option
+          .setName('premise')
+          .setDescription('A brief description of what begins this scene.')
+          .setMaxLength(1500)
+          .setRequired(true)
+      )
+
+      .addIntegerOption((option) =>
+        option
+          .setName('start_year')
+          .setDescription('The in-universe year.')
+          .setMinValue(1)
+          .setMaxValue(999)
+          .setRequired(true)
+      )
+
+      .addStringOption((option) =>
+        option
+          .setName('start_season')
+          .setDescription('The season when the scene begins.')
+          .addChoices(
+            {
+              name: 'Spring',
+              value: 'spring',
+            },
+            {
+              name: 'Summer',
+              value: 'summer',
+            },
+            {
+              name: 'Fall',
+              value: 'fall',
+            },
+            {
+              name: 'Winter',
+              value: 'winter',
+            }
+          )
+          .setRequired(true)
+      )
+
+      .addIntegerOption((option) =>
+        option
+          .setName('start_day')
+          .setDescription('The day of the season, from 1 to 28.')
+          .setMinValue(1)
+          .setMaxValue(28)
+          .setRequired(true)
+      )
+
+      .addStringOption((option) =>
+        option
+          .setName('start_daypart')
+          .setDescription('The general time of day.')
+          .addChoices(
+            {
+              name: 'Morning',
+              value: 'morning',
+            },
+            {
+              name: 'Midmorning',
+              value: 'midmorning',
+            },
+            {
+              name: 'Afternoon',
+              value: 'afternoon',
+            },
+            {
+              name: 'Evening',
+              value: 'evening',
+            },
+            {
+              name: 'Night',
+              value: 'night',
+            },
+            {
+              name: 'Unspecified',
+              value: 'unspecified',
+            }
+          )
+          .setRequired(true)
+      )
+
+      .addStringOption((option) =>
+        option
+          .setName('starting_message')
+          .setDescription('Optional link to the first message of this scene.')
+          .setMaxLength(500)
+      )
+  )
+
+  .addSubcommand((subcommand) =>
+    subcommand
       .setName('view')
       .setDescription('View the scene record for the current thread.')
   )
